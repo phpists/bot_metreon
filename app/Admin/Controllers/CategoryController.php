44eb2023252b2fd884fad3fa3c014154405e545e
mirasoft <mirasoft@mail.ru>
2020-11-01 03:08:53 +0200

    set field subcategory name min:1

--- app/Admin/Controllers/CategoryController.php
+++ app/Admin/Controllers/CategoryController.php
@@ -102,7 +102,7 @@ class CategoryController extends MyAdminController {
 				$form->switch('public'		, __('admin.public'));
 				
 				$form->decimal('sort'		, __('admin.subcategory.sort'))->rules('required');
-				$form->text('name'			, __('admin.subcategory.name'))->rules('required|min:3|max:100');
+				$form->text('name'			, __('admin.subcategory.name'))->rules('required|min:1|max:100');
 			});
 		});
 		
